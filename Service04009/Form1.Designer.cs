e25488a99796e694df923108aeee00b27b24944d
alefismael <alefismael55@gmail.com>
2024-06-18 19:54:22 -0300

    Modifiquei a classe Form para dar já ter uma base do que será a UI do software

--- Service04009/Form1.Designer.cs
+++ Service04009/Form1.Designer.cs
@@ -28,12 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            menuStrip1 = new MenuStrip();
+            atiradoresToolStripMenuItem = new ToolStripMenuItem();
+            exibirToolStripMenuItem = new ToolStripMenuItem();
+            cadastrarToolStripMenuItem = new ToolStripMenuItem();
+            excluirToolStripMenuItem = new ToolStripMenuItem();
+            escalaDeServiçoToolStripMenuItem = new ToolStripMenuItem();
+            serviçoToolStripMenuItem = new ToolStripMenuItem();
+            removerToolStripMenuItem = new ToolStripMenuItem();
+            gerarToolStripMenuItem = new ToolStripMenuItem();
+            exibirToolStripMenuItem1 = new ToolStripMenuItem();
+            editarToolStripMenuItem = new ToolStripMenuItem();
+            gerardocWordToolStripMenuItem = new ToolStripMenuItem();
+            adicionarToolStripMenuItem = new ToolStripMenuItem();
+            consultarToolStripMenuItem = new ToolStripMenuItem();
+            editarToolStripMenuItem1 = new ToolStripMenuItem();
+            removerToolStripMenuItem1 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(1160, 692);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 19);
+            label1.TabIndex = 0;
+            label1.Text = "by Atr 01 Álef";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(0, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Service04009";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { atiradoresToolStripMenuItem, escalaDeServiçoToolStripMenuItem, serviçoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.RenderMode = ToolStripRenderMode.System;
+            menuStrip1.Size = new Size(1280, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // atiradoresToolStripMenuItem
+            // 
+            atiradoresToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { exibirToolStripMenuItem, cadastrarToolStripMenuItem, excluirToolStripMenuItem, removerToolStripMenuItem });
+            atiradoresToolStripMenuItem.Name = "atiradoresToolStripMenuItem";
+            atiradoresToolStripMenuItem.Size = new Size(73, 20);
+            atiradoresToolStripMenuItem.Text = "Atiradores";
+            // 
+            // exibirToolStripMenuItem
+            // 
+            exibirToolStripMenuItem.Name = "exibirToolStripMenuItem";
+            exibirToolStripMenuItem.Size = new Size(124, 22);
+            exibirToolStripMenuItem.Text = "Cadastrar";
+            // 
+            // cadastrarToolStripMenuItem
+            // 
+            cadastrarToolStripMenuItem.Name = "cadastrarToolStripMenuItem";
+            cadastrarToolStripMenuItem.Size = new Size(124, 22);
+            cadastrarToolStripMenuItem.Text = "Exibir";
+            // 
+            // excluirToolStripMenuItem
+            // 
+            excluirToolStripMenuItem.Name = "excluirToolStripMenuItem";
+            excluirToolStripMenuItem.Size = new Size(124, 22);
+            excluirToolStripMenuItem.Text = "Atualizar";
+            // 
+            // escalaDeServiçoToolStripMenuItem
+            // 
+            escalaDeServiçoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { gerarToolStripMenuItem, exibirToolStripMenuItem1, editarToolStripMenuItem, gerardocWordToolStripMenuItem });
+            escalaDeServiçoToolStripMenuItem.Name = "escalaDeServiçoToolStripMenuItem";
+            escalaDeServiçoToolStripMenuItem.Size = new Size(108, 20);
+            escalaDeServiçoToolStripMenuItem.Text = "Escala de Serviço";
+            // 
+            // serviçoToolStripMenuItem
+            // 
+            serviçoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { adicionarToolStripMenuItem, consultarToolStripMenuItem, editarToolStripMenuItem1, removerToolStripMenuItem1 });
+            serviçoToolStripMenuItem.Name = "serviçoToolStripMenuItem";
+            serviçoToolStripMenuItem.Size = new Size(57, 20);
+            serviçoToolStripMenuItem.Text = "Serviço";
+            // 
+            // removerToolStripMenuItem
+            // 
+            removerToolStripMenuItem.Name = "removerToolStripMenuItem";
+            removerToolStripMenuItem.Size = new Size(124, 22);
+            removerToolStripMenuItem.Text = "Remover";
+            // 
+            // gerarToolStripMenuItem
+            // 
+            gerarToolStripMenuItem.Name = "gerarToolStripMenuItem";
+            gerarToolStripMenuItem.Size = new Size(180, 22);
+            gerarToolStripMenuItem.Text = "Criar";
+            // 
+            // exibirToolStripMenuItem1
+            // 
+            exibirToolStripMenuItem1.Name = "exibirToolStripMenuItem1";
+            exibirToolStripMenuItem1.Size = new Size(180, 22);
+            exibirToolStripMenuItem1.Text = "Exibir";
+            // 
+            // editarToolStripMenuItem
+            // 
+            editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            editarToolStripMenuItem.Size = new Size(180, 22);
+            editarToolStripMenuItem.Text = "Editar";
+            // 
+            // gerardocWordToolStripMenuItem
+            // 
+            gerardocWordToolStripMenuItem.Name = "gerardocWordToolStripMenuItem";
+            gerardocWordToolStripMenuItem.Size = new Size(180, 22);
+            gerardocWordToolStripMenuItem.Text = "Gerar .doc Word";
+            // 
+            // adicionarToolStripMenuItem
+            // 
+            adicionarToolStripMenuItem.Name = "adicionarToolStripMenuItem";
+            adicionarToolStripMenuItem.Size = new Size(180, 22);
+            adicionarToolStripMenuItem.Text = "Adicionar";
+            // 
+            // consultarToolStripMenuItem
+            // 
+            consultarToolStripMenuItem.Name = "consultarToolStripMenuItem";
+            consultarToolStripMenuItem.Size = new Size(180, 22);
+            consultarToolStripMenuItem.Text = "Consultar";
+            // 
+            // editarToolStripMenuItem1
+            // 
+            editarToolStripMenuItem1.Name = "editarToolStripMenuItem1";
+            editarToolStripMenuItem1.Size = new Size(180, 22);
+            editarToolStripMenuItem1.Text = "Editar";
+            // 
+            // removerToolStripMenuItem1
+            // 
+            removerToolStripMenuItem1.Name = "removerToolStripMenuItem1";
+            removerToolStripMenuItem1.Size = new Size(180, 22);
+            removerToolStripMenuItem1.Text = "Remover";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lime;
+            ClientSize = new Size(1280, 720);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem atiradoresToolStripMenuItem;
+        private ToolStripMenuItem exibirToolStripMenuItem;
+        private ToolStripMenuItem cadastrarToolStripMenuItem;
+        private ToolStripMenuItem excluirToolStripMenuItem;
+        private ToolStripMenuItem escalaDeServiçoToolStripMenuItem;
+        private ToolStripMenuItem serviçoToolStripMenuItem;
+        private ToolStripMenuItem removerToolStripMenuItem;
+        private ToolStripMenuItem gerarToolStripMenuItem;
+        private ToolStripMenuItem exibirToolStripMenuItem1;
+        private ToolStripMenuItem editarToolStripMenuItem;
+        private ToolStripMenuItem gerardocWordToolStripMenuItem;
+        private ToolStripMenuItem adicionarToolStripMenuItem;
+        private ToolStripMenuItem consultarToolStripMenuItem;
+        private ToolStripMenuItem editarToolStripMenuItem1;
+        private ToolStripMenuItem removerToolStripMenuItem1;
     }
 }
