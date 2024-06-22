3ce555020d4bcdc1e3937d054b3d4a8c135204fb
alefismael <alefismael55@gmail.com>
2024-06-22 12:34:16 -0300

    criei um Mutex para não permitir que esse programa esteja com duas instâncias em execução, muito útil para impedir falhas que poderia acontecer por acessar o arquivo de sqlite ao mesmo tempo.

--- Service04009/Program.cs
+++ Service04009/Program.cs
@@ -2,12 +2,26 @@ namespace Service04009
 {
     internal static class Program
     {
+
+        private static Mutex? mutex = null;
+        private const string MutexName = "YourUniqueAppNameMutex";
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            bool createdNew;
+            mutex = new Mutex(true, MutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                // Se o Mutex já existir, significa que outra instância está em execução
+                MessageBox.Show("O aplicativo já está em execução.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
