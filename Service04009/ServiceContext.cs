97eebbd3e17c17f5c03ae2c554fb4ae83ce1ab2b
alefismael <alefismael55@gmail.com>
2024-06-24 20:44:55 -0300

    Implementei todo o relacionamento entre as classes.

--- Service04009/ServiceContext.cs
+++ Service04009/ServiceContext.cs
@@ -5,13 +5,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Service04009
-{
-    internal class ServiceContext : DbContext
-    {
-        public DbSet<Shooter> Shooters { get; set; }
+namespace Service04009;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source=shooter.db");
+internal class ServiceContext : DbContext
+{
+    public DbSet<Shooter> Shooters { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<ServiceScale> ServiceScales { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlite($"Data Source=shooter.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Shooter>().HasMany(s => s.CommanderServices).WithOne(s => s.CommanderOfTheGuard).HasForeignKey(s => s.CommanderOfTheGuardId);
+
+        modelBuilder.Entity<Service>().HasMany(s => s.Sentinels).WithMany(s => s.SentinelServices).UsingEntity("ServiceSentinelRelational"); ;
+
+        modelBuilder.Entity<Service>().HasMany(s => s.Permanences).WithMany(s => s.PermanenceServices).UsingEntity("ServicePermanenceRelational"); ;
+
+        modelBuilder.Entity<ServiceScale>().HasMany(s => s.Services).WithOne(s => s.ServiceScale).HasForeignKey(s => s.ServiceScaleId);
     }
 }
+
